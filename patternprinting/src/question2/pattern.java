91f3dc1719ac13a5488f987ce347990b16f7af23
ASUS <ASUS@LAPTOP-I7HFO4GD>
2022-08-21 13:53:27 +0530

    patternprint

---
+++ patternprinting/src/question2/pattern.java
@@ -0,0 +1,19 @@
+package question2;
+
+public class pattern {
+	public static void main(String[] args) {
+		int a = 7;
+		int i = 0;
+		int j;
+		for (i=0;i<=a;i++) {
+			for (j=0;j<i;j++) {
+				System.out.print(i*j+" ");
+
+			}
+			System.out.println("  ");	
+
+		}
+
+	}
+
+}
